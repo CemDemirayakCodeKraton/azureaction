489203f361f2c437346a66754399d4a5dd0d885a
cemdemirayak <145120566+cemdemirayak@users.noreply.github.com>
2025-01-28 11:00:17 +0100

    FC

---
+++ src/main/java/org/example/azureaction/AzureactionApplication.java
@@ -0,0 +1,13 @@
+package org.example.azureaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureactionApplication.class, args);
+    }
+
+}
