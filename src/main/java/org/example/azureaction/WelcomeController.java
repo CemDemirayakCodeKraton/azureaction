949efce9bce45a068e7d99543026f71a336777d2
cemdemirayak <145120566+cemdemirayak@users.noreply.github.com>
2025-01-28 11:10:10 +0100

    Update WelcomeController.java

--- src/main/java/org/example/azureaction/WelcomeController.java
+++ src/main/java/org/example/azureaction/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping
     public String welcome() {
-        return "Hello World!";
+        return "Hello From Azure Action!";
     }
 }
